e37312f4d6cbd9348e5f23072e65634831620f28
Igor Batukhtin <131672373+ibatukhtin@users.noreply.github.com>
2024-11-22 15:49:46 +0500

    fixes

--- js/scrollTo.js
+++ js/scrollTo.js
@@ -2,12 +2,16 @@ const toRegistrations = document.querySelectorAll('.to-registration');
 
 toRegistrations.forEach(
   (toRegistration, i) => {
-    toRegistration.addEventListener("click", () => document.getElementById('registration').scrollIntoView({ behavior: 'smooth' }))
+    toRegistration
+      .addEventListener("click", () =>
+        document.getElementById('registration').scrollIntoView({ behavior: 'smooth', block: "center" }))
   });
 
 const toContacts = document.querySelectorAll('.to-contacts');
 
 toContacts.forEach(
   (toRegistration, i) => {
-    toRegistration.addEventListener("click", () => document.getElementById('footer').scrollIntoView({ behavior: 'smooth' }))
+    toRegistration
+      .addEventListener("click", () =>
+        document.getElementById('footer').scrollIntoView({ behavior: 'smooth' }))
   });
